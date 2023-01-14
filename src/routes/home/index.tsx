106ca29e21b117c2aa4e26e728b354a8a28de01c
r3dDoX <r3dDoX@users.noreply.github.com>
2023-01-14 16:24:25 +0100

    add method to remove ingredients and add more styling

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,14 +1,22 @@
-import {$, component$, useStore, useTask$} from '@builder.io/qwik';
+import {$, component$, useSignal, useStore, useTask$} from '@builder.io/qwik';
 import {DocumentHead} from '@builder.io/qwik-city';
 
 export default component$(() => {
   const state = useStore({
-    ingredients: [] as Array<string>,
-    input: '',
+    ingredients: [] as string[],
     fetchData: false,
     recipes: undefined,
   });
-  const updateIngredients = $(() => state.ingredients = [...state.ingredients, state.input]);
+  const inputRef = useSignal<HTMLInputElement>();
+  const addIngredient = $(() => {
+    if (inputRef.value && inputRef.value.value.trim() !== '') {
+      state.ingredients = [...state.ingredients, inputRef.value!.value];
+      inputRef.value.value = '';
+    }
+  });
+  const removeIngredient = $((ingredientToRemove: string) => {
+    state.ingredients = state.ingredients.filter(ingredient => ingredient !== ingredientToRemove);
+  })
 
   useTask$(async ({track}) => {
     track(() => state.fetchData);
@@ -31,28 +39,38 @@ export default component$(() => {
       <label for="ingredientInput" className="block text-sm font-medium text-gray-700">Ingredient:&nbsp;</label>
       <input
         id="ingredientInput"
+        ref={inputRef}
         type="text"
-        class="my-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-        value={state.input}
-        onInput$={(ev) => {
-          state.input = (ev.target as HTMLInputElement).value;
+        class="my-2 relative z-10 rounded-l-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        onKeyDown$={({key}) => {
+          if (key === 'Enter') {
+            addIngredient();
+          }
         }}
       />
-      <div>
-        {state.ingredients?.map((value, i) => (
-          <div key={i}>{value}</div>
-        ))}
-      </div>
       <button
         type="button"
-        className="rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        onClick$={updateIngredients}
+        class="rounded-r-md border border-l-0 border-gray-300 bg-white py-2.5 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        onClick$={addIngredient}
       >
         add
       </button>
+      <ul class="my-1">
+        {state.ingredients?.map((value, i) => (
+          <li key={i} class="flex items-center group">
+            {value}
+            <button
+              class="ml-2 rounded-md border border-gray-300 bg-white text-xs px-2 hidden group-hover:inline-block"
+              onClick$={() => removeIngredient(value)}
+            >
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
       <button
         type="button"
-        className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         onClick$={() => state.fetchData = true}
       >
         search recipes
